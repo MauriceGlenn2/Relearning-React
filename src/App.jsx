0505fc755c79b0c91e8fbc61fe1af062d62c82d7
Maurice <mauriceglenn2@gmail.com>
2023-11-03 10:22:44 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Todo from "./components/Todo"
+import Header from "./components/Header";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      </div>
+  );
+}
+
+// can export to use
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+//Creating component, create folder to store, creat function to export out
+//Reusable
+function Todo(){
+    return(
+        <div>
+            <h2>Finish FES</h2>
+            <button>Delete</button>
+        </div>
+    )
+}
+
+export default Todo;
